f89f96ea7f92e7d28f546755c6397a5ebfdd88e5
Junko Tahara <junkotahara911@gmail.com>
2020-03-16 16:53:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -51,7 +51,7 @@ def third_challenge
         {
           :name => "Steven",
           :age => "30",
-          :attitude => "worried"
+          :attitude => "confused"
         },
         {
           :name => "Mercutio",
