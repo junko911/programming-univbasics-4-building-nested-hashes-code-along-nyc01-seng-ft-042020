e0deb878c5132f337f71e7c4ea1b8919af1c670f
Junko Tahara <junkotahara911@gmail.com>
2020-03-16 16:46:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,10 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
-      patriarch
+      :patriarch => { },
+      :matriarch => { },
+      :heroine => { },
+      :heroine_friends => [ ]
       }}
 
   
